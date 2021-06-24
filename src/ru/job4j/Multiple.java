c83a78f90e8aeff475146d6e6944505740c17a25
55 <qars2010@yandex.ru>
2021-06-24 14:28:52 +0500

    Multiple.java

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main ( String [] args) {
+
+        int result = 1 ;
+        for ( int i = 0; i < 10;++i) {
+
+            System.out.println( "1 x " + i +" = " + result * i );
+        }
+    }
+}
---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main ( String [] args) {
+        System.out.println("Sergei Sandrakov");
+        System.out.println("25.10.1998");
+    }
+}
