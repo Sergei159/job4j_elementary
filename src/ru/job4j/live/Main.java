54045a01f5be3409990ffcf7c3ca549aeca216bd
55 <qars2010@yandex.ru>
2021-07-06 11:30:21 +0500

    Генерация конструкций в IDEA [#279134]

---
+++ src/ru/job4j/live/Main.java
@@ -0,0 +1,10 @@
+package ru.job4j.live;
+
+public class Main {
+    public static void main(String[] args) {
+        int size = 10;
+        for (int i = 0; i <= size; i++) {
+            System.out.println(i);
+        }
+    }
+}
