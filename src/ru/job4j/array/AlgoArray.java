9f9b65dc200a5cdd5ab1e1f0eda2c8b2bbfeceaa
55 <qars2010@yandex.ru>
2021-07-06 16:00:10 +0500

    Упорядочить массив. [#4441]

---
+++ src/ru/job4j/array/AlgoArray.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        boolean isSorted = false;
+
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
