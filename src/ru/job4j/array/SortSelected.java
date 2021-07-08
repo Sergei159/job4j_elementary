b4f3836067af506edd38ffd3199208d8a8ee0ac0
55 <qars2010@yandex.ru>
2021-07-08 15:02:30 +0500

    Сортировка выборкой. [#156320 #175331]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -6,11 +6,8 @@ public class SortSelected {
         for (int step = 0; step < data.length - 1; step++) {
             int min = MinDiapason.findMin(data, step, data.length - 1);
             int index = FindLoop.indexOf(data, min, step, data.length - 1);
-
-            int temp = data[index];
-            data[index] = data[step];
-            data[step] = temp;
-        }
+            data = SwitchArray.swap(data, index, step);
+           }
             return data;
     }
 }
